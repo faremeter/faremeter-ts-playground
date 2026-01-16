72ec9b5d4d7f7a01931517a901ecb0b68e2d593f
Alexander Guy <alexander.guy@pm.me>
2026-01-16 14:36:31 -0800

    Update repo config to mimic infra-toolbox

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,28 +1,47 @@
 // @ts-check
+
 import * as eslint from "@eslint/js";
 import * as tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const buildFinished = ".build-finished";
+
+const checkTypes =
+  process.env.INSIDE_STAGING_DIR !== "true" &&
+  fs.existsSync(path.join(import.meta.dirname, buildFinished));
 
 export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.strictTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-    globalIgnores(["**/idl_type.ts", "**/dist/**"]),
-    {
-        rules: {
-            "no-console": "error",
-            "@typescript-eslint/consistent-type-definitions": 0,
-            "@typescript-eslint/restrict-template-expressions": 0,
-            "@typescript-eslint/no-confusing-void-expression": 0,
-            "@typescript-eslint/require-await": 0,
-            "@typescript-eslint/no-unnecessary-condition": 0,
-            "@typescript-eslint/no-unsafe-argument": 0,
+  eslint.configs.recommended,
+  checkTypes ? tseslint.configs.strictTypeChecked : tseslint.configs.strict,
+  checkTypes
+    ? tseslint.configs.stylisticTypeChecked
+    : tseslint.configs.stylistic,
+  globalIgnores(["**/idl_type.ts", "**/dist/**"]),
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": 0,
+      "@typescript-eslint/restrict-template-expressions": 0,
+      "@typescript-eslint/no-confusing-void-expression": 0,
+      "@typescript-eslint/require-await": 0,
+      "@typescript-eslint/no-unnecessary-condition": 0,
+      "@typescript-eslint/no-unsafe-argument": 0,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
         },
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+      ],
     },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
 );
